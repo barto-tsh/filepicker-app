18a35fbcaca9b7916c5241498670dbd7080c7f9b
Bartosz Gajos <bartosz.gajos@tsh.io>
2022-05-16 16:46:40 +0200

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import { FilePicker } from '@furgonetka/ui';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const ourCallback = (event) => {
+  console.log(event)
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const rootNode = document.getElementById('root');
+ReactDOM.render(
+  <>
+    <FilePicker fileUploadApiBaseUrl={'https://api-test.furgonetka.pl'} onChange={ourCallback} />
+  </>, rootNode);
+
+
