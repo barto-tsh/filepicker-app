cbcc718031d86d05f76b34891c7ed2de60aa1ef1
Bartosz Gajos <bartosz.gajos@tsh.io>
2022-05-16 16:56:47 +0200

    add comment

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { FilePicker } from '@furgonetka/ui';
 
+/** here handle your callback */
 const ourCallback = (event) => {
-  console.log(event)
-}
+  console.log(event);
+};
 
 const rootNode = document.getElementById('root');
 ReactDOM.render(
